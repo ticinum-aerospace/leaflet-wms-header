842fac6dac7879bc3b542942e72a75963a705c12
D. Galeazzo <dgaleazzo@gmail.com>
2018-12-21 10:38:34 +0100

    ...

--- index.js
+++ index.js
@@ -17,25 +17,25 @@ function callAjax(url, callback, headers) {
 }
 
 L.TileLayer.WMSHeader = L.TileLayer.WMS.extend({
-	initialize: function (url, options, headers) {
-		L.TileLayer.WMS.prototype.initialize.call(this, url, options);
-		this.headers = headers;
-	},
-	createTile(coords, done) {
-		const url = this.getTileUrl(coords);
-		const img = document.createElement('img');
-		callAjax(
-			url,
-			(response) => {
-				img.src = URL.createObjectURL(response);
-				done(null, img);
-			},
-			this.headers
-		)
-		return img;
-	}
+  initialize: function (url, options, headers) {
+    L.TileLayer.WMS.prototype.initialize.call(this, url, options);
+    this.headers = headers;
+  },
+  createTile(coords, done) {
+    const url = this.getTileUrl(coords);
+    const img = document.createElement('img');
+    callAjax(
+      url,
+      (response) => {
+        img.src = URL.createObjectURL(response);
+        done(null, img);
+      },
+      this.headers
+    )
+    return img;
+  }
 });
 
 L.TileLayer.wmsHeader = function (url, options, headers) {
-	return new L.TileLayer.WMSHeader(url, options, headers);
+  return new L.TileLayer.WMSHeader(url, options, headers);
 }
