61e3f9f8fe0e54f3b576bc677ddf260d96832da4
Jonas Ellehauge <joel@dhigroup.com>
2019-09-18 13:49:06 +0200

    Rewrite to use `fetch` & avoid `URL.createObjectURL`

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,20 @@
-import * as L from 'leaflet';
+import * as L from "leaflet";
 
-
-declare module 'leaflet' {
+declare module "leaflet" {
     namespace TileLayer {
         export class WMSHeader extends WMS {
             constructor(
                 baseUrl: string,
                 options: WMSOptions,
-                header: {header: string, value: string}[],
+                header: { header: string; value: string }[],
                 abort: Promise<void>
             );
         }
-    }
-    namespace tileLayer {
+
         export function wmsHeader(
             baseUrl: string,
             options: WMSOptions,
-            header: {header: string, value: string}[],
-            abort: Promise<void>
+            header: { header: string; value: string }[]
         ): WMSHeader;
     }
 }
